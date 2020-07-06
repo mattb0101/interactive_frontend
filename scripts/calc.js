9a000f0a3d65b10d22d6ee258815d0c42eed6d51
mattb0101 <mattbaum666@googlemail.com>
2020-07-06 09:28:56 +0000

    FizzBuzz  test. put this in with code from solution as there were a couple more things

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,20 +1,38 @@
-function whatCanIDrink(age) {
-    if (age < 0) {
-        return "Sorry. I can't tell you what to drink because that age is incorrect!";
-    } else if (age < 14) {
-        return "Drink Toddy";
-    } else if (age < 18 && age >=14){
-        return "Drink Coke";
-    } else if (age < 21 && age >=18) {
-        return "Drink Beer";
-    } else if (age < 130 && age >=21) {
-        return "Drink Whiskey";
-    } else if (age > 130) { 
-        return "Sorry. I can't tell you what to drink because that age is incorrect!";
-    } else {   
-        return "Error!";
-    } 
+function fizzBuzz(number) {
+    if (typeof(number) === "number" && (number % 3) === 0 && (number % 5) == 0) {
+        return "FizzBuzz"; 
+    } else if (typeof(number) === "number" && (number % 5) == 0) {
+        return "Buzz";
+    } else if (typeof(number) === "number" && (number % 3) == 0) {
+        return "Fizz";
+    } else if (typeof(number) === "number") {
+        return number;
+    } else {
+        return "Not a number!";
+    }
 }
+
+
+    
+
+// Function for drinking age.
+// function whatCanIDrink(age) {
+//     if (age < 0) {
+//         return "Sorry. I can't tell you what to drink because that age is incorrect!";
+//     } else if (age < 14) {
+//         return "Drink Toddy";
+//     } else if (age < 18 && age >=14){
+//         return "Drink Coke";
+//     } else if (age < 21 && age >=18) {
+//         return "Drink Beer";
+//     } else if (age < 130 && age >=21) {
+//         return "Drink Whiskey";
+//     } else if (age > 130) { 
+//         return "Sorry. I can't tell you what to drink because that age is incorrect!";
+//     } else {   
+//         return "Error!";
+//     } 
+// }
 
 
 // Updated with Running Total
--- spec/calcSpec.js
+++ spec/calcSpec.js
@@ -1,29 +1,87 @@
-
-describe("whatICanDrink", function() {
-    describe("Age Tests", function() {
-        it("Should Return Drink Toddy", function() {
-            expect(whatCanIDrink(12)).toBe("Drink Toddy");
+describe("fizzBuzz", function() {
+    describe("Number Tests", function() {
+        it("Should return Fizz", function() {
+            expect(fizzBuzz(6)).toBe("Fizz");
         });
-         it("Should Return Drink Coke", function() {
-            expect(whatCanIDrink(14)).toBe("Drink Coke");
+        it("Should return Buzz", function() {
+            expect(fizzBuzz(10)).toBe("Buzz");
         });
-        it("Should Return Drink Beer", function() {
-            expect(whatCanIDrink(18)).toBe("Drink Beer");
+        it("Should return FizzBuzz", function() {
+            expect(fizzBuzz(15)).toBe("FizzBuzz");
         });
-        it("Should Return Drink Whiskey", function() {
-            expect(whatCanIDrink(54)).toBe("Drink Whiskey");
+        it("Should return number", function() {
+            expect(fizzBuzz(11)).toBe(11);
         });
-        it("Should return a statement if age is over 130", function() {
-            expect(whatCanIDrink(131)).toBe("Sorry. I can't tell you what to drink because that age is incorrect!");
-        });
-        it("Should return a statement if age is less than 0", function() {
-            expect(whatCanIDrink(-5)).toBe("Sorry. I can't tell you what to drink because that age is incorrect!");
-        });
-        it("Should return an error if value is not a number", function() {
-            expect(whatCanIDrink("None of your Business")).toBe("Error!");
+        it("Should return an error", function() {
+            expect(fizzBuzz("chicken")).toBe("Not a number!");
         });
     });
 });
+
+// describe("My fizzBuzz function", function() {
+//     // A test suite begins with a call to the global Jasmine function describe with two parameters: a string and a function.
+    
+//     beforeEach(function() {
+//         fizzbuzz = new fizzBuzz();
+//     });
+    
+//     describe("Returns number, fizz, buzz or fizzbuzz", function() {
+//         // Specs are defined by calling the global Jasmine function it
+//         it("should exist", function() {
+//             // Expectations are built with the function expect which takes a value, called the actual. 
+//             // Each matcher implements a boolean comparison between the actual value and the expected value.
+//             // Any matcher can evaluate to a negative assertion by chaining the call to expect with a not before calling the matcher.
+//             expect(fizzBuzz).toBeDefined();
+//         });
+        
+//         it("should return Fizz when called as fizzBuzz(9)", function() {
+//             var result = fizzBuzz(9)
+//             expect(result).toBe("Fizz");
+//         });
+                
+//         it("should return Buzz when called as fizzBuzz(10)", function() {
+//             var result = fizzBuzz(10)
+//             expect(result).toBe("Buzz");
+//         });
+        
+//         it("should return FizzBuzz when called as fizzBuzz(15)", function() {
+//             var result = fizzBuzz(15)
+//             expect(result).toBe("FizzBuzz");
+//         });
+//         it("should return 2 when called as fizzBuzz(2)", function() {
+//             var result = fizzBuzz(2)
+//             expect(result).toBe(2);
+//         });
+//     });
+// });
+
+
+// Tests for drinking age
+// describe("whatICanDrink", function() {
+//     describe("Age Tests", function() {
+//         it("Should Return Drink Toddy", function() {
+//             expect(whatCanIDrink(12)).toBe("Drink Toddy");
+//         });
+//          it("Should Return Drink Coke", function() {
+//             expect(whatCanIDrink(14)).toBe("Drink Coke");
+//         });
+//         it("Should Return Drink Beer", function() {
+//             expect(whatCanIDrink(18)).toBe("Drink Beer");
+//         });
+//         it("Should Return Drink Whiskey", function() {
+//             expect(whatCanIDrink(54)).toBe("Drink Whiskey");
+//         });
+//         it("Should return a statement if age is over 130", function() {
+//             expect(whatCanIDrink(131)).toBe("Sorry. I can't tell you what to drink because that age is incorrect!");
+//         });
+//         it("Should return a statement if age is less than 0", function() {
+//             expect(whatCanIDrink(-5)).toBe("Sorry. I can't tell you what to drink because that age is incorrect!");
+//         });
+//         it("Should return an error if value is not a number", function() {
+//             expect(whatCanIDrink("None of your Business")).toBe("Error!");
+//         });
+//     });
+// });
 
 
 // Test for Calculator
